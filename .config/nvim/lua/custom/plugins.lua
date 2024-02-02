edf1a3df55c4dbf3f5ea3230aa7587239d13b8e1
AZarbade <anzarbade@gmail.com>
2024-02-02 22:44:08 +0530

    added python support to nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,46 +1,38 @@
 local plugins = {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "python",
+        "rust",
+        "json",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function ()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function ()
-      vim.g.rustfmt_autosave = 1
-    end
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
-      return require "custom.configs.rust-tools"
-    end,
-    config = function (_, opts)
-    end
-  },
-  {
-    "mfussenegger/nvim-dap",
-  },
-  {
-    "saecki/crates.nvim",
-    ft = {"rust", "toml"},
-    config = function (_, opts)
-      local crates = require('crates')
-      crates.setup(opts)
-      crates.show()
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function ()
-      local M = require "plugins.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
-      return M
     end,
   },
 }
