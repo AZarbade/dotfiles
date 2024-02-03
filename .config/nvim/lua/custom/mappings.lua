5628cd12e4b8133a656c908623e8e188d0846dcf
AZarbade <anzarbade@gmail.com>
2024-02-03 08:42:57 +0530

    added github-copilot setup

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,3 +1,14 @@
 local M = {}
 
+M.copilot = {
+  i = {
+    ["<C-a>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
 return M
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -35,5 +35,14 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    end
+  },
 }
 return plugins
