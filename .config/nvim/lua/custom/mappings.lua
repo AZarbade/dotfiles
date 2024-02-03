da1ac1ebf63a29b15e41b463f4bb19fa83be41bd
AZarbade <anzarbade@gmail.com>
2024-02-04 00:07:24 +0530

    removed copilot plugin

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,14 +1,3 @@
 local M = {}
 
-M.copilot = {
-  i = {
-    ["<C-a>"] = {
-      function()
-        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-      end,
-      "Copilot Accept",
-      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
-    }
-  }
-}
 return M
