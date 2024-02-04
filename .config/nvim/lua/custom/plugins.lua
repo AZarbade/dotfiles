a2fb53d4f8500d5764553869a5d4eff68976ac31
AZarbade <anzarbade@gmail.com>
2024-02-05 00:06:51 +0530

    added rust support to nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -4,7 +4,7 @@ local plugins = {
     opts = {
       ensure_installed = {
         "lua",
-        "python",
+        -- "python",
         "rust",
         "json",
       },
@@ -15,7 +15,8 @@ local plugins = {
     opts = {
       ensure_installed = {
         "lua-language-server",
-        "pyright",
+        -- "pyright",
+        "rust-analyzer",
       },
     },
   },
@@ -24,6 +25,24 @@ local plugins = {
     config = function ()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require("custom.configs.rust-tools")
+    end,
+    config = function (_, opts)
+      require('rust-tools').setup(opts)
     end,
   },
   {
