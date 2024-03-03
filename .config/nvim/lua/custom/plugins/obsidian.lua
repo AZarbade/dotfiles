255bf2d822d78a2ca49ac9bdb4d9ff1a18703c62
AZarbade <anzarbade@gmail.com>
2024-03-03 19:38:16 +0530

    added vimbegood, markdown preview, obsidian, fugitive remove autopairs

--- .config/nvim/lua/custom/plugins/obsidian.lua
+++ .config/nvim/lua/custom/plugins/obsidian.lua
@@ -28,12 +28,13 @@ return {
           opts = { noremap = false, expr = true, buffer = true },
         },
         -- Toggle check-boxes.
-        ['<leader>ch'] = {
+        ['<leader>oc'] = {
           action = function()
             return require('obsidian').util.toggle_checkbox()
           end,
           opts = { buffer = true },
         },
+        vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<CR>'),
       },
       new_notes_location = 'notes_subdir',
       follow_url_func = function(url)
@@ -43,6 +44,22 @@ return {
       ui = {
         enable = false,
       },
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ''
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. '-' .. suffix
+      end,
     }
   end,
 }
