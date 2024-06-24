433be1cdf652bdb48b55b089f74ec72496111fde
AZarbade <adwaitzarbade@gmail.com>
2024-06-24 11:02:52 +0530

    house keeping

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,7 @@
 set -g fish_greeting
 set -g fish_vi_key_bindings
-# set -g fish_default_key_bindings
 
 fish_add_path /opt/nvim-linux64/bin
-fish_add_path /bin/exercism
 source "$HOME/.asdf/asdf.fish"
 atuin init fish | source
 
@@ -12,7 +10,6 @@ abbr -a ls exa -l
 abbr -a lsl exa -la
 abbr -a tree exa --tree
 abbr -a tm tmux -u
-abbr -a spd speedtest-rs
 abbr -a btop btop --utf-force
 abbr -a gg git-graph
 
@@ -20,11 +17,12 @@ abbr -a gg git-graph
 abbr -a homelab ssh onyx@homelab.local
 abbr -a broker ssh noir@broker.local
 
-# Embedded Rust bindings
+# Embedded bindings
 abbr -a get_esprs . $HOME/export-esp.sh
-abbr -a get_idf . $HOME/personal/embedded_rtos/esp-idf/export.fish
+abbr -a get_idf . $HOME/personal/esp_box/esp-idf/export.fish
+abbr -a idf idf.py
 
-# Poetry helpers
+# Poetry (python) helpers
 abbr -a pos poetry shell
 abbr -a por poetry run python
 
