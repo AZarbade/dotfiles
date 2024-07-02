7db908b7872990184f538c8a228cb494190350c4
AZarbade <adwaitzarbade@gmail.com>
2024-07-02 13:55:44 +0530

    new config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,5 @@
--- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-
-require 'options'
-require 'keymaps'
-require 'lazy-bootstrap'
-require 'lazy-plugins'
-
--- vim: ts=2 sts=2 sw=2 et
+require("config.keymaps")
+require("config.options")
+require("config.autocmds")
+require("config.abbreviations")
+require("config.lazy")
