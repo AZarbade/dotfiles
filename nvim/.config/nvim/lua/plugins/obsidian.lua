c73e243e3bc94a5438843b00609cb842555f64b6
AZarbade <adwaitzarbade@gmail.com>
2024-07-03 21:24:00 +0530

    added obsidian. config pending

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,43 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = {
+    "BufReadPre $HOME/personal/notes/**.md",
+    "BufNewFile $HOME/personal/notes/**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/personal/notes",
+      },
+    },
+  },
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+    -- Smart action depending on context, either follow link or toggle checkbox.
+    ["<cr>"] = {
+      action = function()
+        return require("obsidian").util.smart_action()
+      end,
+      opts = { buffer = true, expr = true },
+    },
+  },
+}
