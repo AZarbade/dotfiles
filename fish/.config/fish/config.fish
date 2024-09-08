4fedbb0c8573882b2ad07981ee3edbaeeee1706f
AZarbade <anzarbade@gmail.com>
2024-09-08 21:23:54 +0530

    fish: changed greetings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -114,3 +114,22 @@ function fzf_tmux
         tmux new-session -s $selected_name -c "$selected"
     end
 end
+
+# Fish prompt
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostnamectl hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
