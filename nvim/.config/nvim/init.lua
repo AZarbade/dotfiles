a7e200a8e6bd4d4a72d2848ec79373fcbcd7a627
AZarbade <anzarbade@gmail.com>
2024-09-24 10:15:25 +0530

    lsp tweaks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -284,6 +284,54 @@ require("lazy").setup({
 		"neovim/nvim-lspconfig",
 		dependencies = {},
 		config = function()
+			-- UI Customization
+			-- Option #1
+			local border = {
+				{ "╭", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "╮", "FloatBorder" },
+				{ "│", "FloatBorder" },
+				{ "╯", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "╰", "FloatBorder" },
+				{ "│", "FloatBorder" },
+			}
+
+			-- Option #2
+			-- local border = {
+			-- 	{ "┌", "FloatBorder" },
+			-- 	{ "─", "FloatBorder" },
+			-- 	{ "┐", "FloatBorder" },
+			-- 	{ "│", "FloatBorder" },
+			-- 	{ "┘", "FloatBorder" },
+			-- 	{ "─", "FloatBorder" },
+			-- 	{ "└", "FloatBorder" },
+			-- 	{ "│", "FloatBorder" },
+			-- }
+
+			-- To instead override globally
+			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+				opts = opts or {}
+				opts.border = opts.border or border
+				return orig_util_open_floating_preview(contents, syntax, opts, ...)
+			end
+
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = false,
+			})
+
+			local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
+
+			-- servers
 			require("lspconfig").clangd.setup({}) -- enable clangd (esp-idf)
 			-- require("lspconfig").ccls.setup({}) -- enable ccls (platformio)
 
