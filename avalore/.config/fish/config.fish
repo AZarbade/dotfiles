fe4cb4648bf683db2c920c7192098cc592d43eff
AZarbade <anzarbade@gmail.com>
2024-10-03 18:57:55 +0530

    update fish_greetings

--- avalore/.config/fish/config.fish
+++ avalore/.config/fish/config.fish
@@ -38,13 +38,6 @@ abbr -a gc 'git commit'
 abbr -a gm 'git merge --no-ff'
 abbr -a gg 'git-graph'
 
-# Tor browser
-abbr -a tor start_tor
-function start_tor
-	cd ~/Documents/tor-browser
-	./start-tor-browser.desktop
-end
-
 # Obsidian setup
 abbr -a oo obsidian_notes
 function obsidian_notes
@@ -66,14 +59,12 @@ function fish_greeting
 	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
-	
+
 	echo -e " \\e[1mDisk usage:\\e[0m"
 	echo
 	echo -ne (\
 		df -l -h | grep -E '/home|dev/(xvda|sd|mapper)' | \
-		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
-		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
-		paste -sd ''\
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}'
 	)
 	echo
 
