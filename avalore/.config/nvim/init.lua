2361c3ff517513d80d97240d2d15ba2c1ba59ac7
AZarbade <anzarbade@gmail.com>
2024-10-08 19:29:52 +0530

    readded obsidian.nvim (not configured yet)

--- avalore/.config/nvim/init.lua
+++ avalore/.config/nvim/init.lua
@@ -529,4 +529,21 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/personal/notes",
+				},
+			},
+		},
+	},
 })
