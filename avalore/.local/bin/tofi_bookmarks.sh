e29b8a5ab6ee4a4ee4208bf7bb7a85624730eeec
AZarbade <anzarbade@gmail.com>
2024-10-20 09:17:25 +0530

    fix: fixed pathing bug

--- avalore/.local/bin/tofi_bookmarks.sh
+++ avalore/.local/bin/tofi_bookmarks.sh
@@ -2,24 +2,17 @@
 
 # Path to your URL file
 url_file="$HOME/.bookmarks"
-
-# Check if the file exists
 if [ ! -f "$url_file" ]; then
     echo "URL file not found: $url_file"
     exit 1
 fi
 
-# Read URLs from the file
+# Read URLs from the file and display in tofi
 mapfile -t urls < "$url_file"
-
-# Use tofi to display the URLs and capture the selection
 selected=$(printf '%s\n' "${urls[@]}" | tofi --prompt="Select a URL: ")
 
-# Check if a URL was selected
 if [ -n "$selected" ]; then
-    # Extract the URL part (before the #)
     url=$(echo "$selected" | awk -F' #' '{print $1}')
-    
-    # Copy the URL to clipboard using wl-copy
     echo -n "$url" | wl-copy
+	wtype -M ctrl v -m ctrl
 fi
