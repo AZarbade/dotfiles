c9cd93a6764c07768895e4dd72b3bfda7cf4c642
AZarbade <anzarbade@gmail.com>
2024-11-30 19:14:52 +0530

    feat: added nvim config

---
+++ ros_ws/.config/nvim/init.lua
@@ -0,0 +1,546 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+
+-------------------------------------------------------------------------------
+--
+-- preferences
+--
+-------------------------------------------------------------------------------
+-- never ever folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = "manual"
+vim.opt.foldlevelstart = 99
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 5
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = "yes"
+-- sweet sweet relative line numbers
+vim.opt.relativenumber = true
+-- and show the absolute line number for the current line
+vim.opt.number = true
+-- keep current content top + left when splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- infinite undo!
+-- NOTE: ends up in ~/.local/state/nvim/undo/
+vim.opt.undofile = true
+--" Decent wildmenu
+-- in completion, when there is more than one match,
+-- list all matches, and only complete to longest common match
+vim.opt.wildmode = "list:longest"
+-- when opening a file with a command (like :e),
+-- don't suggest files like there:
+vim.opt.wildignore = ".hg,.svn,*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,vendor,dist,_site"
+-- tabs: go big or go home
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+-- unless uppercase in search term
+vim.opt.smartcase = true
+-- never ever make my terminal beep
+vim.opt.vb = true
+-- more useful diffs (nvim -d)
+--- by ignoring whitespace
+vim.opt.diffopt:append("iwhite")
+--- and using a smarter algorithm
+--- https://vimways.org/2018/the-power-of-diff/
+--- https://stackoverflow.com/questions/32365271/whats-the-difference-between-git-diff-patience-and-git-diff-histogram
+--- https://luppeng.wordpress.com/2020/10/10/when-to-use-each-of-the-git-diff-algorithms/
+vim.opt.diffopt:append("algorithm:histogram")
+vim.opt.diffopt:append("indent-heuristic")
+-- show more hidden characters
+-- also, show tabs nicer
+vim.opt.listchars = "tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•"
+-- conceal level
+vim.opt.conceallevel = 2
+
+-------------------------------------------------------------------------------
+--
+-- hotkeys
+--
+-------------------------------------------------------------------------------
+
+-- make missing : less annoying
+vim.keymap.set("n", ";", ":")
+-- Ctrl+q to stop searching
+vim.keymap.set("v", "<C-q>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<C-q>", "<cmd>nohlsearch<cr>")
+-- Copy to system clipboard
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+-- Paste from system clipboard
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })
+-- Delete to void
+vim.keymap.set("n", "<leader>d", '"_d', { noremap = true, silent = true })
+-- <leader><leader> toggles between buffers
+vim.keymap.set("n", "<leader><leader>", "<c-^>")
+-- always center search results
+vim.keymap.set("n", "n", "nzz", { silent = true })
+vim.keymap.set("n", "N", "Nzz", { silent = true })
+vim.keymap.set("n", "*", "*zz", { silent = true })
+vim.keymap.set("n", "#", "#zz", { silent = true })
+vim.keymap.set("n", "g*", "g*zz", { silent = true })
+-- "very magic" (less escaping needed) regexes by default
+vim.keymap.set("n", "?", "?\\v")
+vim.keymap.set("n", "/", "/\\v")
+vim.keymap.set("c", "%s/", "%sm/")
+-- open new file adjacent to current file
+vim.keymap.set("n", "<C-o>", ':e <C-R>=expand("%:p:h") . "/" <cr>')
+-- open file in vertical mode
+vim.keymap.set("n", "<C-i>", ':vsplit <C-R>=expand("%:p:h") . "/" <cr>')
+-- no arrow keys --- force yourself to use the home row
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+vim.keymap.set("i", "<up>", "<nop>")
+vim.keymap.set("i", "<down>", "<nop>")
+vim.keymap.set("i", "<left>", "<nop>")
+vim.keymap.set("i", "<right>", "<nop>")
+-- let the left and right arrows be useful: they can switch buffers
+vim.keymap.set("n", "<left>", ":bp<cr>")
+vim.keymap.set("n", "<right>", ":bn<cr>")
+-- make j and k move by visual line, not actual line, when text is soft-wrapped
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+-- close current buffer
+vim.keymap.set("n", "Q", ":bd<CR>")
+-- Move selected lines up
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+-- Unload current buffer
+vim.keymap.set("n", "Q", ":bd<CR>")
+-- manual format
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- substitute word under cursor (current buffer)
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Switch between panels using Ctrl + hjkl
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+-- RustLsp keybinds (need "mrcjkb/rustaceanvim")
+vim.keymap.set("n", "<leader>do", ":RustLsp openDocs<CR>")
+
+-------------------------------------------------------------------------------
+--
+-- autocommands
+--
+-------------------------------------------------------------------------------
+-- highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	command = "silent! lua vim.highlight.on_yank({ timeout = 500 })",
+})
+-- jump to last edit position on opening file
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function(ev)
+		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			-- except for in git commit messages
+			-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+			if not vim.fn.expand("%:p"):find(".git", 1, true) then
+				vim.cmd('exe "normal! g\'\\""')
+			end
+		end
+	end,
+})
+-- prevent accidental writes to buffers that shouldn't be edited
+vim.api.nvim_create_autocmd("BufRead", { pattern = "*.orig", command = "set readonly" })
+vim.api.nvim_create_autocmd("BufRead", { pattern = "*.pacnew", command = "set readonly" })
+-- leave paste mode when leaving insert mode (if it was on)
+vim.api.nvim_create_autocmd("InsertLeave", { pattern = "*", command = "set nopaste" })
+--- tex has so much syntax that a little wider is ok
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = "tex",
+	group = text,
+	command = "setlocal spell tw=80 colorcolumn=81",
+})
+
+-- text wrapping for markdown files
+vim.api.nvim_create_augroup("markdown_settings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.textwidth = 80
+		vim.opt_local.colorcolumn = "80"
+	end,
+})
+
+-------------------------------------------------------------------------------
+--
+-- plugin configuration
+--
+-------------------------------------------------------------------------------
+-- first, grab the manager
+-- https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+-- then, setup!
+require("lazy").setup({
+	-- main color scheme
+	{
+		"uZer/pywal16.nvim",
+		lazy = false, -- load at start
+		priority = 1000, -- load first
+		config = function()
+			local pywal16 = require("pywal16")
+			pywal16.setup()
+			local pywal16_core = require("pywal16.core")
+			local colors = pywal16_core.get_colors()
+		end,
+	},
+	-- nice bar at the bottom
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"meuter/lualine-so-fancy.nvim",
+		},
+		enabled = true,
+		lazy = false,
+		event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "pywal16-nvim",
+					globalstatus = true,
+					icons_enabled = true,
+					component_separators = { left = "|", right = "|" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {
+							"help",
+						},
+					},
+				},
+				sections = {
+					lualine_a = {
+						{ "fancy_branch" },
+					},
+					lualine_b = {
+						{
+							"filename",
+							path = 1,
+							symbols = {
+								modified = "  ",
+								readonly = "  ",
+								unnamed = "  ",
+							},
+						},
+						{
+							"fancy_diagnostics",
+							sources = { "nvim_lsp" },
+							symbols = { error = " ", warn = " ", info = " " },
+						},
+						{ "fancy_searchcount" },
+					},
+					lualine_c = {},
+					lualine_x = {
+						"fancy_lsp_servers",
+					},
+					lualine_y = {
+						{ "fancy_filetype" },
+					},
+					lualine_z = {},
+				},
+				extensions = { "lazy" },
+			})
+		end,
+	},
+	-- quick navigation
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").create_default_mappings()
+		end,
+	},
+	-- telescope for fzf utils
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+			{ "<C-g>", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+			{ "<leader>;", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		},
+	},
+	-- LSP
+	-- NOTE: for LSP servers, using system installed binaries.
+	-- This is because esp-idf requires a custom "clangd", which mason does not have.
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {},
+		config = function()
+			-- UI Customization
+			local border = {
+				{ "╭", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "╮", "FloatBorder" },
+				{ "│", "FloatBorder" },
+				{ "╯", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "╰", "FloatBorder" },
+				{ "│", "FloatBorder" },
+			}
+
+			-- To instead override globally
+			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+				opts = opts or {}
+				opts.border = opts.border or border
+				return orig_util_open_floating_preview(contents, syntax, opts, ...)
+			end
+
+			vim.diagnostic.config({
+				virtual_text = false,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = false,
+			})
+
+			local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
+
+			-- servers
+			require("lspconfig").clangd.setup({}) -- enable clangd (esp-idf)
+			require("lspconfig").ruff.setup({}) -- enable ruff (python)
+			require("lspconfig").rust_analyzer.setup({}) -- enable rust analyzer (rust)
+
+			-- Global mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				end,
+			})
+		end,
+	},
+	-- Lua support
+	{
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+		{ -- optional completion source for require statements and module annotations
+			"hrsh7th/nvim-cmp",
+			opts = function(_, opts)
+				opts.sources = opts.sources or {}
+				table.insert(opts.sources, {
+					name = "lazydev",
+					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+				})
+			end,
+		},
+	},
+	-- code completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					-- REQUIRED by nvim-cmp. get rid of it once we can
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = {
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-y>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+				},
+				-- Installed sources:
+				sources = {
+					{ name = "path" }, -- file paths
+					{ name = "nvim_lsp", keyword_length = 3 }, -- from language server
+					{ name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
+					{ name = "buffer", keyword_length = 2 }, -- source current buffer
+				},
+				formatting = {
+					fields = { "menu", "abbr", "kind" },
+					format = function(entry, item)
+						local menu_icon = {
+							nvim_lsp = "λ",
+							buffer = "Ω",
+						}
+						item.menu = menu_icon[entry.source.name]
+						return item
+					end,
+				},
+				experimental = {
+					ghost_text = false,
+				},
+			})
+			-- Enable completing paths in :
+			cmp.setup.cmdline(":", {
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+	-- what the lsp doing!
+	{
+		"vigoux/notifier.nvim",
+		config = function()
+			require("notifier").setup({})
+		end,
+	},
+	-- code formatting
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					-- stylua should be installed manually to the system.
+					lua = { "stylua" },
+					rust = { "rustfmt" }, -- install rustfmt via `rustup`.
+				},
+				format_on_save = {
+					lsp_format = "fallback",
+				},
+			})
+		end,
+	},
+	-- enables colors in json files. usefull for colorscheme tweaks
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				"json",
+			})
+		end,
+	},
+	-- Git changes besides number column
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+
+					-- Actions
+					map("n", "<leader>hs", gitsigns.stage_hunk)
+					map("n", "<leader>hr", gitsigns.reset_hunk)
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("n", "<leader>hp", gitsigns.preview_hunk)
+					map("n", "<leader>hd", gitsigns.diffthis)
+					map("n", "<leader>td", gitsigns.toggle_deleted)
+				end,
+			})
+		end,
+	},
+	-- Obsidian and Markdown stuff
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/personal/notes",
+				},
+			},
+			ui = {
+				enable = true,
+			},
+		},
+	},
+})
