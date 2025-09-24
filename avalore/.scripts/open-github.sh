3a1b47cf81a5e1a43528315cac6b36eda4abae71
Adwait Zarbade <anzarbade@gmail.com>
2025-09-24 17:51:57 +0530

    fix: now actually opens webpage (on linux)

--- avalore/.scripts/open-github.sh
+++ avalore/.scripts/open-github.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/bash
-
-cd $(tmux run "echo #{pane_start_path}")
-url=$(get remote get-url origin)
-
-open $url || echo "No remote found"
+#!/usr/bin/bash
+cd $(tmux run "echo #{pane_start_path}") && \
+git remote get-url origin | sed 's/git@\([^:]*\):\(.*\)\.git$/https:\/\/\1\/\2/' | xargs xdg-open
